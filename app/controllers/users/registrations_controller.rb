26286d2ab1c4b2c95e962c337f8927997c34dee2
anand_chandak <anandchandak52@gmail.com>
2024-01-24 16:10:15 +0530

    last changes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,11 +8,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def respond_with(current_user, _opts = {})
   
     if resource.persisted?
+
       render json: {
+        
         status: {code: 200, message: 'Signed up successfully.',
         data: { user: current_user }
         }
-      },status: :ok
+      }, status: :ok
     else
       render json: {
         status: {message: "User couldn't be created successfully. #{current_user.errors.full_messages.to_sentence}"}
