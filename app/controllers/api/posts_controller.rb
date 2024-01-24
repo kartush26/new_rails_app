bea405d1214a1c669d52a9f27359dfdc9fa9a818
anand_chandak <anandchandak52@gmail.com>
2024-01-24 16:45:04 +0530

    1st commit added

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -14,9 +14,7 @@ class Api::PostsController < ApplicationController
   
     def create
       @post = Post.new(post_params)
-  
       if @post.save
-        
         render json: @post, status: :created
       else
         render json: @post.errors, status: :unprocessable_entity
